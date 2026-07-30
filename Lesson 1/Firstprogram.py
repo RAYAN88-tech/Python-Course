d8e656c43d9fe2c9296bb00ea0d0feceab04a985
RAYAN88-tech <rayanazmat11@gmail.com>
2026-07-30 16:55:03 +0500

    Add files via upload

---
+++ Lesson 1/Firstprogram.py
@@ -0,0 +1,73 @@
+# ==================================
+# My Personal Profile
+# ==================================
+
+# Personal Information
+name = "Rayan Azmat"
+age = 15
+grade = 10
+school = "The Scholars"
+city = "Dgkhan"
+country = "Pakistan"
+
+# Family Information
+father_name = "Muhammad Azmat"
+mother_name = "Majida Muhammad"
+siblings = 2
+
+# Favorite Things
+favorite_subject = "Computer Science"
+favorite_color = "Black and White"
+favorite_food = "Pizza"
+favorite_drink = "Water"
+favorite_book = "Attack on titan Manga"
+favorite_game = "Football"
+favorite_hobby = "Playing Minecraft"
+favorite_animal = "Cat"
+
+# Dream and Goals
+dream_job = "entrepreneurship"
+goal = "Make a succesful company"
+
+# School Information
+class_teacher = "Mrs. Sameena"
+school_timing = "6:30 AM - 10:40 AM"
+
+# Marks
+engilsh = 85
+math= 92
+science = 89 
+computer = 95
+urdu = 75
+
+print("=================================")
+print("     My Personal Profile")
+print("=================================")
+
+print("Name:", name)
+print("Age:", age)
+print("Grade:", grade)
+print("School:", school)
+print("City:", city)
+print("Country:", country)
+
+print("------------------------------------")
+print("Family Information")
+print("------------------------------------")
+
+print("Father's Name:", father_name)
+print("Mother's Name:", mother_name)
+print("Number of Siblings:", siblings)
+
+print("----------------------------------")
+print("Favorite Things")
+print("----------------------------------")
+
+print("Favorite Subject:", favorite_subject)
+print("Favorite Color:", favorite_color)
+print("Favorite Food:", favorite_food)
+print("Favorite Drink:", favorite_drink)
+print("Favorite Book:", favorite_book)
+print("Favorite Game:", favorite_game)
+print("Favorite Hobby:", favorite_hobby)
+print("Favorite Animal:", favorite_animal)
